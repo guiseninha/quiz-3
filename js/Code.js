3332272ce6ea94bbb1f92d0e9e57479ff16d71fb
guiseninha <107883094+guiseninha@users.noreply.github.com>
2022-12-20 15:58:53 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "ANO NOVO";
+const accessCode2 = "PASCOA";
+const accessCode3 = "NATAL";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("A O N V N O O", 100,50)
+    fill("lightblue")
+    text("Dica: É a primeira data festiva do ano", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C S A P O A", 700,150)
+    fill("lightblue")
+    text("Dica: É a segunda data festiva do ano", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("L T A N A", 100,250)
+    fill("lightblue")
+    text("Dica: Ultima data festiva do ano", 100,270)
+
+}
